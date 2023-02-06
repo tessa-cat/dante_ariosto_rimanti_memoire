d6e790c9cbad4f245dde8f2483609bae022c68d5
tessa-cat <82821292+tessa-cat@users.noreply.github.com>
2023-02-06 12:18:10 +0100

    update

--- tesi_master/calculations.py
+++ tesi_master/calculations.py
@@ -20,56 +20,57 @@ def main():
     OrlandoFurioso = open('orlando_rimanti.json')
     of_rimanti_list = json.load(OrlandoFurioso)
 
-    # # INFERNO
-    # value_inferno = 0
-    # for canto_D in inferno_rimanti_list : 
-    #     # Index of each canto in the list
-    #     canto_D_index = inferno_rimanti_list.index(canto_D)
-    #     # Accessing the specific canto to cycle through
-    #     canto_D_to_analyse = inferno_rimanti_list[canto_D_index]
+    # INFERNO
+    value_inferno = 0
+    for canto_D in inferno_rimanti_list : 
+        # Index of each canto in the list
+        canto_D_index = inferno_rimanti_list.index(canto_D)
+        # Accessing the specific canto to cycle through
+        canto_D_to_analyse = inferno_rimanti_list[canto_D_index]
 
-    #     #Cycling through each canto of Orlando Furioso
-    #     for canto_A in of_rimanti_list:
-    #         canto_A_index = of_rimanti_list.index(canto_A)
-    #         canto_A_to_analyse = of_rimanti_list[canto_A_index]
-    #         # First we cycle through the sublists of rimanti in the specific Dante canto
-    #         for sub_list_D in canto_D_to_analyse:
-    #             # Determining the index of the sub_list in question because otherwise I will get lost
-    #             if_index = canto_D_to_analyse.index(sub_list_D)
+        #Cycling through each canto of Orlando Furioso
+        for canto_A in of_rimanti_list:
+            canto_A_index = of_rimanti_list.index(canto_A)
+            canto_A_to_analyse = of_rimanti_list[canto_A_index]
+            # First we cycle through the sublists of rimanti in the specific Dante canto
+            for sub_list_D in canto_D_to_analyse:
+                # Determining the index of the sub_list in question because otherwise I will get lost
+                if_index = canto_D_to_analyse.index(sub_list_D)
 
-    #             # First and last pair of rimanti of each canto
-    #             if if_index == 0 or if_index == (len(canto_D_to_analyse) - 1):
-    #             # Cycling through the list of the rimanti in the Orlando Furioso to check against
-    #                 for sub_list_A in canto_A_to_analyse:
-    #                     # Again index of each sublist to not get lost
-    #                     of_index = canto_A_to_analyse.index(sub_list_A)
-    #                     # This statement is true if both the rimanti in the Dante's canto are found in one of the sub_list of rimanti of the Ariosto canto
-    #                     if canto_D_to_analyse[if_index][0] in canto_A_to_analyse[of_index] and canto_D_to_analyse[if_index][1] in canto_A_to_analyse[of_index]:
-    #                         #print(canto_D_to_analyse[if_index][0], canto_D_to_analyse[if_index][1], canto_A_to_analyse[of_index], canto_D_index, canto_A_index)
-    #                         value_inferno += 1/3
+                # First and last pair of rimanti of each canto
+                if if_index == 0 or if_index == (len(canto_D_to_analyse) - 1):
+                # Cycling through the list of the rimanti in the Orlando Furioso to check against
+                    for sub_list_A in canto_A_to_analyse:
+                        # Again index of each sublist to not get lost
+                        of_index = canto_A_to_analyse.index(sub_list_A)
+                        # This statement is true if both the rimanti in the Dante's canto are found in one of the sub_list of rimanti of the Ariosto canto
+                        if canto_D_to_analyse[if_index][0] in canto_A_to_analyse[of_index] and canto_D_to_analyse[if_index][1] in canto_A_to_analyse[of_index]:
+                            #print(canto_D_to_analyse[if_index][0], canto_D_to_analyse[if_index][1], canto_A_to_analyse[of_index], canto_D_index, canto_A_index)
+                            value_inferno += 1/3
 
-    #             # Sub_lists of three rimanti
-    #             elif if_index != 0 and if_index != (len(canto_D_to_analyse) - 1):
-    #                 for sub_list_A in canto_A_to_analyse:
-    #                     # Again index of each sublist to not get lost
-    #                     of_index = canto_A_to_analyse.index(sub_list_A)
-    #                     # The sub_lists are as follows: [A, B, C]
-    #                     # 1 complete point assigned if all three are in the OF sub_list
-    #                     # First case, A and B are in one of the OF sub_lists (1/3 points)
-    #                     if canto_D_to_analyse[if_index][0] in canto_A_to_analyse[of_index] and canto_D_to_analyse[if_index][1] in canto_A_to_analyse[of_index]:
-    #                         #print(canto_D_to_analyse[if_index][0], canto_D_to_analyse[if_index][1],canto_A_to_analyse[of_index], canto_D_index, canto_A_index)
-    #                         value_inferno += 1/3
-    #                     # Second case, A and C are in one of the OF sub_lists (1/3 points)
-    #                     if canto_D_to_analyse[if_index][0] in canto_A_to_analyse[of_index] and canto_D_to_analyse[if_index][2] in canto_A_to_analyse[of_index]:
-    #                         #print(canto_D_to_analyse[if_index][0], canto_D_to_analyse[if_index][2],canto_A_to_analyse[of_index], canto_D_index, canto_A_index)
-    #                         value_inferno += 1/3
-    #                     # Third case, B and C are in one of the OF sub_lists (1/3 points)
-    #                     if canto_D_to_analyse[if_index][1] in canto_A_to_analyse[of_index] and canto_D_to_analyse[if_index][2] in canto_A_to_analyse[of_index]:
-    #                         #print(canto_D_to_analyse[if_index][1], canto_D_to_analyse[if_index][2],canto_A_to_analyse[of_index], canto_D_index, canto_A_index)
-    #                         value_inferno += 1/3
+                # Sub_lists of three rimanti
+                elif if_index != 0 and if_index != (len(canto_D_to_analyse) - 1):
+                    for sub_list_A in canto_A_to_analyse:
+                        # Again index of each sublist to not get lost
+                        of_index = canto_A_to_analyse.index(sub_list_A)
+                        # The sub_lists are as follows: [A, B, C]
+                        # 1 complete point assigned if all three are in the OF sub_list
+                        # First case, A and B are in one of the OF sub_lists (1/3 points)
+                        if canto_D_to_analyse[if_index][0] in canto_A_to_analyse[of_index] and canto_D_to_analyse[if_index][1] in canto_A_to_analyse[of_index]:
+                            #print(canto_D_to_analyse[if_index][0], canto_D_to_analyse[if_index][1],canto_A_to_analyse[of_index], canto_D_index, canto_A_index)
+                            value_inferno += 1/3
+                        # Second case, A and C are in one of the OF sub_lists (1/3 points)
+                        if canto_D_to_analyse[if_index][0] in canto_A_to_analyse[of_index] and canto_D_to_analyse[if_index][2] in canto_A_to_analyse[of_index]:
+                            #print(canto_D_to_analyse[if_index][0], canto_D_to_analyse[if_index][2],canto_A_to_analyse[of_index], canto_D_index, canto_A_index)
+                            value_inferno += 1/3
+                        # Third case, B and C are in one of the OF sub_lists (1/3 points)
+                        if canto_D_to_analyse[if_index][1] in canto_A_to_analyse[of_index] and canto_D_to_analyse[if_index][2] in canto_A_to_analyse[of_index]:
+                            #print(canto_D_to_analyse[if_index][1], canto_D_to_analyse[if_index][2],canto_A_to_analyse[of_index], canto_D_index, canto_A_index)
+                            value_inferno += 1/3
     
-    # tot_inferno = (value_inferno/33894)*100
-    # print(tot_inferno)
+    tot_inferno = (value_inferno/33894)*100
+    tot_inferno_strg = str(tot_inferno)
+    print('Inferno: '+tot_inferno_strg)
 
     # PURGATORIO
     value_purgatorio = 0
@@ -120,8 +121,64 @@ def main():
                             value_purgatorio += 1/3
     
     tot_purgatorio = (value_purgatorio/33894)*100
-    print(tot_purgatorio)
+    tot_purgatorio_str = str(tot_purgatorio)
+    print('Purgatorio: '+tot_purgatorio_str)
+
     # PARADISO
+    value_paradiso = 0
+    for canto_D in paradiso_rimanti_list : 
+        # Index of each canto in the list
+        canto_D_index = paradiso_rimanti_list.index(canto_D)
+        # Accessing the specific canto to cycle through
+        canto_D_to_analyse = paradiso_rimanti_list[canto_D_index]
+
+        #Cycling through each canto of Orlando Furioso
+        for canto_A in of_rimanti_list:
+            canto_A_index = of_rimanti_list.index(canto_A)
+            canto_A_to_analyse = of_rimanti_list[canto_A_index]
+            # First we cycle through the sublists of rimanti in the specific Dante canto
+            for sub_list_D in canto_D_to_analyse:
+                # Determining the index of the sub_list in question because otherwise I will get lost
+                pd_index = canto_D_to_analyse.index(sub_list_D)
+
+                # First and last pair of rimanti of each canto
+                if pd_index == 0 or pd_index == (len(canto_D_to_analyse) - 1):
+                # Cycling through the list of the rimanti in the Orlando Furioso to check against
+                    for sub_list_A in canto_A_to_analyse:
+                        # Again index of each sublist to not get lost
+                        of_index = canto_A_to_analyse.index(sub_list_A)
+                        # This statement is true if both the rimanti in the Dante's canto are found in one of the sub_list of rimanti of the Ariosto canto
+                        if canto_D_to_analyse[pd_index][0] in canto_A_to_analyse[of_index] and canto_D_to_analyse[pd_index][1] in canto_A_to_analyse[of_index]:
+                            #print(canto_D_to_analyse[pg_index][0], canto_D_to_analyse[pg_index][1], canto_A_to_analyse[of_index], canto_D_index, canto_A_index)
+                            value_paradiso += 1/3
+
+                # Sub_lists of three rimanti
+                elif pd_index != 0 and pd_index != (len(canto_D_to_analyse) - 1):
+                    for sub_list_A in canto_A_to_analyse:
+                        # Again index of each sublist to not get lost
+                        of_index = canto_A_to_analyse.index(sub_list_A)
+                        # The sub_lists are as follows: [A, B, C]
+                        # 1 complete point assigned if all three are in the OF sub_list
+                        # First case, A and B are in one of the OF sub_lists (1/3 points)
+                        if canto_D_to_analyse[pd_index][0] in canto_A_to_analyse[of_index] and canto_D_to_analyse[pd_index][1] in canto_A_to_analyse[of_index]:
+                            #print(canto_D_to_analyse[pg_index][0], canto_D_to_analyse[pg_index][1],canto_A_to_analyse[of_index], canto_D_index, canto_A_index)
+                            value_paradiso += 1/3
+                        # Second case, A and C are in one of the OF sub_lists (1/3 points)
+                        if canto_D_to_analyse[pd_index][0] in canto_A_to_analyse[of_index] and canto_D_to_analyse[pd_index][2] in canto_A_to_analyse[of_index]:
+                            #print(canto_D_to_analyse[pg_index][0], canto_D_to_analyse[pg_index][2],canto_A_to_analyse[of_index], canto_D_index, canto_A_index)
+                            value_paradiso += 1/3
+                        # Third case, B and C are in one of the OF sub_lists (1/3 points)
+                        if canto_D_to_analyse[pd_index][1] in canto_A_to_analyse[of_index] and canto_D_to_analyse[pd_index][2] in canto_A_to_analyse[of_index]:
+                            #print(canto_D_to_analyse[pg_index][1], canto_D_to_analyse[pg_index][2],canto_A_to_analyse[of_index], canto_D_index, canto_A_index)
+                            value_paradiso += 1/3
+    
+    tot_paradiso = (value_paradiso/33894)*100
+    tot_paradiso_str = str(tot_paradiso)
+    print('Paradiso: '+tot_paradiso_str)
+
+    totale = ((value_inferno + value_purgatorio + value_paradiso)/33894)*100
+    totale_str = str(totale)
+    print('Totale opera: '+totale_str)
 
 if __name__ == "__main__":
     main()
--- tesi_master/canti_split.py
+++ tesi_master/canti_split.py
@@ -20,24 +20,24 @@ def main():
     #         with open (filepath, 'w') as a:
     #             a.write(item)
 
-    # ## 2. Purgatorio
-    # # Open file
-    # with open ("/Users/tessacattaneo/Desktop/tesi_master/Pd.txt", "r") as Pg:
-    #     Purgatorio = Pg.read()
+    ## 2. Purgatorio
+    # Open file
+    with open ("/Users/tessacattaneo/Desktop/tesi_master/Pg.txt", "r") as Pg:
+        Purgatorio = Pg.read()
 
-    # # Split main file into each canto
-    # canti = re.split('canto\s[ivx]+\n', Purgatorio)
+    # Split main file into each canto
+    canti = re.split('canto\s[ivx]+\n', Purgatorio)
 
-    # # Creating a file for each individual canto
-    # for item in canti:
-    #     index = str((canti.index(item)))
-    #     if index == '1' or index == '2' or index == '3' or index == '4' or index == '5' or index == '6' or index == '7' or index == '8' or index == '9':
-    #         filepath = '/Users/tessacattaneo/Desktop/tesi_master/Purgatorio/0'+index+'_Pg.txt'
-    #     else:
-    #         filepath = '/Users/tessacattaneo/Desktop/tesi_master/Purgatorio/'+index+'_Pg.txt'
-    #     if len(item) != 0:
-    #         with open (filepath, 'w') as a:
-    #             a.write(item)
+    # Creating a file for each individual canto
+    for item in canti:
+        index = str((canti.index(item)))
+        if index == '1' or index == '2' or index == '3' or index == '4' or index == '5' or index == '6' or index == '7' or index == '8' or index == '9':
+            filepath = '/Users/tessacattaneo/Desktop/tesi_master/Purgatorio/0'+index+'_Pg.txt'
+        else:
+            filepath = '/Users/tessacattaneo/Desktop/tesi_master/Purgatorio/'+index+'_Pg.txt'
+        if len(item) != 0:
+            with open (filepath, 'w') as a:
+                a.write(item)
     
     # ## 3. Paradiso
     # # Open file
@@ -58,24 +58,24 @@ def main():
     #         with open (filepath, 'w') as a:
     #             a.write(item)
 
-    ## 4. Orlando Furioso
-    with open ("/Users/tessacattaneo/Desktop/tesi_master/orlando_furioso.txt", "r") as OF:
-        Orlando_furioso = OF.read()
+    # ## 4. Orlando Furioso
+    # with open ("/Users/tessacattaneo/Desktop/tesi_master/orlando_furioso.txt", "r") as OF:
+    #     Orlando_furioso = OF.read()
 
-    # Split main file into each canto
-    canti = re.split('[A-Z]{2,}\s[A-Z]{2,}\n', Orlando_furioso)
+    # # Split main file into each canto
+    # canti = re.split('[A-Z]{2,}\s[A-Z]{2,}\n', Orlando_furioso)
 
-    # Creating a file for each individual canto
-    ## For some reason the before last canto is badly cut --> I cut out the last line manually
-    for item in canti:
-        index = str((canti.index(item)))
-        if index == '1' or index == '2' or index == '3' or index == '4' or index == '5' or index == '6' or index == '7' or index == '8' or index == '9':
-            filepath = '/Users/tessacattaneo/Desktop/tesi_master/OF/0'+index+'_Of.txt'
-        else:
-            filepath = '/Users/tessacattaneo/Desktop/tesi_master/OF/'+index+'_Of.txt'
-        if len(item) != 0:
-            with open (filepath, 'w') as a:
-                a.write(item)
+    # # Creating a file for each individual canto
+    # ## For some reason the before last canto is badly cut --> I cut out the last line manually
+    # for item in canti:
+    #     index = str((canti.index(item)))
+    #     if index == '1' or index == '2' or index == '3' or index == '4' or index == '5' or index == '6' or index == '7' or index == '8' or index == '9':
+    #         filepath = '/Users/tessacattaneo/Desktop/tesi_master/OF/0'+index+'_Of.txt'
+    #     else:
+    #         filepath = '/Users/tessacattaneo/Desktop/tesi_master/OF/'+index+'_Of.txt'
+    #     if len(item) != 0:
+    #         with open (filepath, 'w') as a:
+    #             a.write(item)
 
 if __name__ == "__main__":
     main()
